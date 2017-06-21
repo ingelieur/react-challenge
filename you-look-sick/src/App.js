ed2e65d1f51d658666586bf9851141107cc1754d
ingelieur <ingelieur@gmail.com>
2017-06-21 11:58:09 +0700

    move the methods from grandparent to parent

--- you-look-sick/src/App.js
+++ you-look-sick/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import axios from 'axios'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Header, NameForm, Results, Footer, FailPage } from './components'
 
@@ -7,73 +6,38 @@ class App extends React.Component {
   constructor () {
     super()
     this.state = {
-      tonaldQuote: null,
-      news: null,
       name: null,
-      location: null,
+      loc: null,
       randomFace: null,
       isSubmitted: false,
     }
     this.handleForm = this.handleForm.bind(this)
-    this.tonaldSays = this.tonaldSays.bind(this)
-    this.badNews = this.badNews.bind(this)
   }
+
   componentDidMount () {
     let randomFace = Math.floor(Math.random() * 50000)
     this.setState({
       randomFace
     })
   }
-  tonaldSays (name) {
-    console.log('name ', name)
-    axios.get(`https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=${name}`)
-      .then ((response) => {
-        this.setState({
-          tonaldQuote: response.data.message
-        })
-      })
-      .catch((error) => {
-        this.setState({
-          tonaldQuote: `You are such a terrible human being, even Tonald Drump doesn't want to have any business with you.`
-        })
-      })
-  }
-  badNews (loc) {
-    axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json`, {
-      params: {
-        'api-key': "6bfa079ba439477ead940b11ec70b476",
-        'fq': loc,
-        'sort': 'newest'
-      }
-    })
-      .then ((response) => {
-        this.setState({
-          news: response.data.response.docs
-        })
-      })
-      .catch ((error) => {
-        this.setState({
-          news: `Your country is so sad it's not even on the news.`
-        })
-      })
-  }
+
   handleForm (inputForm) {
     this.setState({
       name: inputForm.name,
-      location: inputForm.location,
+      loc: inputForm.location,
       isSubmitted: true
     }, () => {
-      this.tonaldSays(this.state.name)
-      this.badNews(this.state.location)
+      //setState is asynchronous => it has a callback function
     })
   }
+
   render () {
     return (
       <BrowserRouter>
         <div className="App">
           <Header />
           <Switch>
-            <Route path="/sad-world" component={(props) => <Results news={this.state.news} quote={this.state.tonaldQuote} isSubmitted={this.state.isSubmitted} {...props} />} />
+            <Route path="/sad-world" component={(props) => <Results name={this.state.name} loc={this.state.loc} isSubmitted={this.state.isSubmitted} {...props} />} />
             <Route exact path="/" component={(props) => <NameForm handleForm={this.handleForm} {...props}/>} />
             <Route path="" component={FailPage} />
           </Switch>
--- you-look-sick/src/components/Results.js
+++ you-look-sick/src/components/Results.js
@@ -1,24 +1,84 @@
 import React from 'react'
 import { TonaldSays, News } from '.'
 import { Redirect, Link } from 'react-router-dom'
+import Axios from 'axios'
 
-function Results (props) {
-  if(props.isSubmitted) {
+
+class Results extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      quote: null,
+      news: null,
+    }
+  }
+
+  redir() {
     return (
-      <div>
-        <TonaldSays quote={props.quote}/>
-        <News news={props.news}/>
-        <div className="has-text-centered">
-          <Link to="/" className="button is-primary is-outlined is-large">Start Over</Link>
-        </div>
-      </div>
+      <Redirect to={{
+        pathname: '/'
+      }} />
     )
   }
-  return (
-    <Redirect to={{
-      pathname: '/'
-    }} />
-  )
+
+  componentDidMount () {
+    if(this.props.isSubmitted) {
+      this.tonaldSays(this.props.name)
+      this.badNews(this.props.loc)
+    } else {
+      this.redir()
+    }
+  }
+
+  tonaldSays (name) {
+    Axios.get(`https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=${name}`)
+      .then ((response) => {
+        this.setState({
+          quote: response.data.message
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          quote: `You are such a terrible human being, even Tonald Drump doesn't want to have any business with you.`
+        })
+      })
+  }
+
+  badNews (loc) {
+    Axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json`, {
+      params: {
+        'api-key': "6bfa079ba439477ead940b11ec70b476",
+        'fq': loc,
+        'sort': 'newest'
+      }
+    })
+      .then ((response) => {
+        this.setState({
+          news: response.data.response.docs
+        })
+      })
+      .catch ((error) => {
+        this.setState({
+          news: `Your country is so sad it's not even on the news.`
+        })
+      })
+  }
+
+  render () {
+    if(this.props.isSubmitted) {
+      return (
+        <div>
+          <TonaldSays quote={this.state.quote}/>
+          <News news={this.state.news}/>
+          <div className="has-text-centered">
+            <Link to="/" className="button is-primary is-outlined is-large">Start Over</Link>
+          </div>
+        </div>
+      )
+    }
+    console.log(this.redir);
+    return this.redir()
+  }
 }
 
 export default Results
