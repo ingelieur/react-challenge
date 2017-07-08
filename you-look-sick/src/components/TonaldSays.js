84482657c78a78af850d9afbff9a4dbbb0ee222d
ingelieur <ingelieur@gmail.com>
2017-07-09 02:37:51 +0700

    tidy up ifs

--- you-look-sick/src/components/TonaldSays.js
+++ you-look-sick/src/components/TonaldSays.js
@@ -1,26 +1,42 @@
 import React from 'react'
 
 function TonaldSays (props) {
-  if (props.quote === null) {
-    return (
-      <div className="container has-text-centered">
-        <br />
-        {/*<blockquote className="title has-text-centered">loading ...</blockquote>*/}
-        <a className="button is-large is-loading is-white is-outlined">loading</a>
-        <br />
-      </div>
-    )
-  }
   return (
-    <div className="container">
+    <div className="container has-text-centered">
       <br />
-      <div className="content">
-        <p className="subtitle has-text-centered">This is what Tonald Drump has to say about you: </p>
-        <br />
-        <blockquote className="title has-text-centered">{props.quote}</blockquote>
-      </div>
+      { !props.quote &&
+          <a className="button is-large is-loading is-white is-outlined">loading</a>
+      }
+      { props.quote &&
+          <div className="content">
+            <p className="subtitle has-text-centered">This is what Tonald Drump has to say about you: </p>
+            <br />
+            <blockquote className="title has-text-centered">{props.quote}</blockquote>
+          </div>
+      }
+      <br />
     </div>
   )
+    //  if (props.quote === null) {
+    //    return (
+    //      <div className="container has-text-centered">
+    //        <br />
+    //        {/*<blockquote className="title has-text-centered">loading ...</blockquote>*/}
+    //        <a className="button is-large is-loading is-white is-outlined">loading</a>
+    //        <br />
+    //      </div>
+    //    )
+    //  }
+    //  return (
+    //    <div className="container">
+    //      <br />
+    //      <div className="content">
+    //        <p className="subtitle has-text-centered">This is what Tonald Drump has to say about you: </p>
+    //        <br />
+    //        <blockquote className="title has-text-centered">{props.quote}</blockquote>
+    //      </div>
+    //    </div>
+    //  )
 }
 
 export default TonaldSays
